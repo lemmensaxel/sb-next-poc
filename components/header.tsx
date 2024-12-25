cf0c345430cb83a1d4cbabdf543416842298a2f9
Axel Lemmens <axel.lemmens@ae.be>
2024-12-25 11:47:54 +0100

    Added AE Studio logo

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@
 import { Menubar } from "primereact/menubar";
 import { MenuItem } from "primereact/menuitem";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Header() {
   const router = useRouter();
@@ -22,7 +23,15 @@ export default function Header() {
     <Menubar
       className="h-16 fixed top-0 left-0 right-0 bg-gray-100"
       model={items}
-      start={() => <p className="font-bold mr-4">Nextjs + Storyblok POC</p>}
+      start={() => (
+        <Image
+          src="/logo_studio.svg"
+          width={150}
+          height={64}
+          alt="Logo of AE Studio"
+          priority
+        />
+      )}
     />
   );
 }
